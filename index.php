bac56635fba071be076c94409ce176de7fba04e5
rehmamwamba <147811270+rehmamwamba@users.noreply.github.com>
2024-06-03 13:43:25 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 
 require_once 'foot.php';
 
-$equipe1 = new FootballTeam("Equipe 1", "Coach 1");
-$equipe2 = new FootballTeam("Equipe 2", "Coach 2");
+$equipe1 = new FootballTeam("barca", "messi");
+$equipe2 = new FootballTeam("man u", "vini");
 
 echo "Nom de l'équipe 1 : " . $equipe1->getName() . " ";
 echo "Nom de l'équipe 2 : " . $equipe2->getName() . " ";
